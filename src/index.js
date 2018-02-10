f141432967e630338cc0a4ecb8d52ba4e67ab7a7
Patrick <patrick.x.rivera@gmail.com>
2018-02-09 21:48:16 -0500

    Created new file structure

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>Hello World!</div>
-    )
-  }
-}
+import App from './components/App'
 
 ReactDOM.render(
-  <App />,
+  <App  />,
   document.getElementById('app')
 );
